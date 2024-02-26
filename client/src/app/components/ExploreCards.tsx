ed4160093512ac4b469c9c7ee78f049f9175420f
William Wang <bn.william.w@gmail.com>
2024-02-25 18:02:47 -0700

    better post view

--- client/src/app/components/ExploreCards.tsx
+++ client/src/app/components/ExploreCards.tsx
@@ -8,6 +8,7 @@ interface PostCardProps {
   walletAddress: string;
   comments: number;
   likes: number;
+  imageUrl: string;
 }
 
 const PostCard: React.FC<PostCardProps> = ({
@@ -18,16 +19,15 @@ const PostCard: React.FC<PostCardProps> = ({
   walletAddress,
   comments,
   likes,
+  imageUrl
 }) => {
   return (
     <div className="max-w-sm mx-auto rounded overflow-hidden shadow-lg bg-white p-6 text-black">
+      <div className="text-sm mb-2">Posted by: {walletAddress} to {topic} on {new Date(date).toLocaleDateString()}</div>
+
+      {imageUrl && <img src={imageUrl} alt="Post Image" className="w-full h-48 object-cover mb-4" />}
       <p className="text-gray-700 text-base mb-4">{content}</p>
-      <div className="text-sm mb-2">Topic: {topic}</div>
-      <div className="text-sm mb-2">Post ID: {postId}</div>
-      <div className="text-sm mb-2">
-        Date: {new Date(date).toLocaleDateString()}
-      </div>
-      <div className="text-sm mb-2">Wallet Address: {walletAddress}</div>
+      {/* <div className="text-sm mb-2">Post ID: {postId}</div> */}
       <div className="flex items-center justify-between mt-4">
       <span className="text-sm font-semibold">{likes} Likes</span>
         <span className="text-sm font-semibold">{comments} Comments</span>
